39dfa5bdba7c0afbfe0e81441d78def993d5c996
Vladyslav Rzaiev <vlad.rzaev@gmail.com>
2025-05-16 20:35:15 +0200

    add task-4

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,4 +1,5 @@
-import { getProducts } from '../services/products.js';
+import createHttpError from 'http-errors';
+import { getProduct, getProducts } from '../services/products.js';
 
 export const getProductsController = async (req, res, next) => {
   const products = await getProducts();
@@ -6,5 +7,20 @@ export const getProductsController = async (req, res, next) => {
     status: 200,
     message: 'Successfully found products!',
     data: products,
+  });
+};
+
+export const getProductController = async (req, res) => {
+  const { productId } = req.params;
+  const result = await getProduct(productId);
+
+  if (result === null) {
+    throw createHttpError(404, 'Product not found');
+  }
+
+  res.status(200).json({
+    status: 200,
+    message: `Successfully found product with id ${productId}!`,
+    data: result,
   });
 };
