49a014d5ff6aeb95e965be13eb5b01daa88581c6
pippsza <bastahov18@gmail.com>
2025-05-30 20:22:39 +0300

    test

---
+++ src/middlewares/validId.js
@@ -0,0 +1,10 @@
+import createHttpError from 'http-errors';
+import { isValidObjectId } from 'mongoose';
+
+export default function validateId(req, res, next) {
+  const id = req.params.productId;
+  if (isValidObjectId(id) !== true) {
+    return next(createHttpError.BadRequest('Incorrect id.'));
+  }
+  next();
+}
---
+++ src/middlewares/validateBody.js
@@ -0,0 +1,15 @@
+import createHttpError from 'http-errors';
+
+export default function validateBody(schema) {
+  return async (req, res, next) => {
+    try {
+      await schema.validateAsync(req.body, { abortEarly: false });
+      next;
+    } catch (error) {
+      const errors = error.details.map((el) => {
+        return el.message;
+      });
+      next(createHttpError.BadRequest(errors));
+    }
+  };
+}
