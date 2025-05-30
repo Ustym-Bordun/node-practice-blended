cedc5f79a1948a7e9fcaa415bf1fc9998ce3aefc
pippsza <bastahov18@gmail.com>
2025-05-30 21:09:12 +0300

    123

--- src/routers/products.js
+++ src/routers/products.js
@@ -6,14 +6,30 @@ import {
   updatePrductController,
 } from '../controllers/products.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import validateId from '../middlewares/validId.js';
+import validateBody from '../middlewares/validateBody.js';
+import { productSchema } from '../validation/products.js';
 const router = Router();
 
 router.get('/products', ctrlWrapper(getProductsController));
 
-router.get('/products/:productId', ctrlWrapper(getProductController));
+router.get(
+  '/products/:productId',
+  validateId,
+  ctrlWrapper(getProductController),
+);
 
-router.post('/products', ctrlWrapper(createProductController));
+router.post(
+  '/products',
+  validateBody(productSchema),
+  ctrlWrapper(createProductController),
+);
 
-router.patch('/products/:productId', ctrlWrapper(updatePrductController));
+router.patch(
+  '/products/:productId',
+  validateId,
+  validateBody(productSchema),
+  ctrlWrapper(updatePrductController),
+);
 
 export default router;
